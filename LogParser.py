734870f76b05103f209b3a1e5d1aad6e25cea708
Jakub Baran <kubabaran2000@wp.pl>
2024-05-22 00:12:34 +0200

    random forest implementation

--- LogParser.py
+++ LogParser.py
@@ -13,8 +13,9 @@ class LogParser:
         buckets = []
 
         for i in range(0, df.shape[0], logs_per_bucket):
-            bucket: DataFrame = df.iloc[i:i + logs_per_bucket]
-            bucket['frame-time'] = bucket['frame-time'] - bucket['frame-time'].iloc[0]
-            buckets.append(bucket)
+            if df.shape[0] >= i + logs_per_bucket:
+                bucket: DataFrame = df.iloc[i:i + logs_per_bucket]
+                bucket['frame-time'] = bucket['frame-time'] - bucket['frame-time'].iloc[0]
+                buckets.append(bucket)
 
         return buckets
